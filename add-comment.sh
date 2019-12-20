bf68ca3587e5e9df0d0d90bdaa918b4d80f77b7e
Nam Lu <42857192+nam-lu@users.noreply.github.com>
2019-12-20 15:22:35 +1100

    Remove all check logic to only post gif on execution

--- add-comment.sh
+++ add-comment.sh
@@ -19,111 +19,12 @@ AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
 
 GIF_URL=https://github.com/jessfraz/shaking-finger-action/raw/master/finger.gif
 
-delete_comment_if_exists() {
-	# Get all the comments for the pull request.
-	body=$(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/${GITHUB_REPOSITORY}/issues/${NUMBER}/comments")
-
-	comments=$(echo "$body" | jq --raw-output '.[] | {id: .id, body: .body} | @base64')
-
-	for c in $comments; do
-		comment="$(echo "$c" | base64 --decode)"
-		id=$(echo "$comment" | jq --raw-output '.id')
-		b=$(echo "$comment" | jq --raw-output '.body')
-
-		if [[ "$b" == *"finger.gif"* ]]; then
-			# We have found our comment.
-			# Delete it.
-
-			echo "Deleting old comment ID: $id"
-			curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" -X DELETE "${URI}/repos/${GITHUB_REPOSITORY}/issues/comments/${id}"
-		fi
-	done
-}
-
 post_gif() {
 	curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" -d '{"body":"![finger.gif]('${GIF_URL}')"}' -H "Content-Type: application/json" -X POST "${URI}/repos/${GITHUB_REPOSITORY}/issues/${NUMBER}/comments"
 }
 
-get_checks() {
-	echo "start get_checks()"
-	# Get all the checks for the sha.
-	body=$(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/${GITHUB_REPOSITORY}/commits/${GITHUB_SHA}/check-runs")
-	echo "AUTH_HEADER: ${AUTH_HEADER}"
-	echo "API_HEADER: ${API_HEADER}"
-	echo "URI: ${URI}"
-	echo "GITHUB_REPOSITORY: ${GITHUB_REPOSITORY}"
-	echo "GITHUB_SHA: ${GITHUB_SHA}"
-	echo "------------------------------------------------------------"
-	echo "Body: $body"
-	echo "------------------------------------------------------------"
-
-	checks=$(echo "$body" | jq --raw-output '.check_runs | .[] | {name: .name, status: .status, conclusion: .conclusion} | @base64')
-	echo "Checks: $checks"
-	echo "------------------------------------------------------------"
-
-	IN_PROGRESS=0
-	for c in $checks; do
-		check="$(echo "$c" | base64 --decode)"
-		name=$(echo "$check" | jq --raw-output '.name')
-		state=$(echo "$check" | jq --raw-output '.status')
-		conclusion=$(echo "$check" | jq --raw-output '.conclusion')
-
-		if [[ "$GITHUB_ACTION" == "$name" ]]; then
-			# Continue if it's us.
-			continue
-		fi
-
-		if [[ "$state" == "in_progress" ]]; then
-			# Continue if it's in progress.
-			IN_PROGRESS=1
-			continue
-		fi
-
-		if [[ "$state" == "completed" ]] && [[ "$conclusion" == "failure" ]]; then
-			echo "Check: $name failed. Posting gif..."
-
-			delete_comment_if_exists;
-			post_gif;
-
-			exit 0
-		fi
-	done
-
-	# If we got in progress checks then sleep and loop again.
-	if [[ "$IN_PROGRESS" == "1" ]]; then
-		echo "In progress loop. Sleeping..."
-		sleep 2
-
-		get_checks;
-	fi
-
-	# We made it to the end and nothing failed so let's delete the comment if it
-	# exists.
-	delete_comment_if_exists;
-	echo "finish get_checks()"
-}
-
 main() {
-	# Validate the GitHub token.
-	curl -o /dev/null -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/${GITHUB_REPOSITORY}" || { echo "Error: Invalid repo, token or network issue!";  exit 1; }
-
-	# Get the check run action.
-	action=$(jq --raw-output .action "$GITHUB_EVENT_PATH")
-
-	# If it's not synchronize or opened event return early.
-	if [[ "$action" != "synchronize" ]] && [[ "$action" != "opened" ]]; then
-		# Return early we only care about synchronize or opened.
-		echo "Check run has action: $action"
-		echo "Want: synchronize or opened"
-		exit 0
-	fi
-
-	# Get the pull request number.
-	NUMBER=$(jq --raw-output .number "$GITHUB_EVENT_PATH")
-
-	echo "running $GITHUB_ACTION for PR #${NUMBER}"
-
-	get_checks;
+	post_gif;
 }
 
 main
