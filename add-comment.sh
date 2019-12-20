883bf8f861cba5afb8bccdc4bdb4a904bb768cab
Nam Lu <42857192+nam-lu@users.noreply.github.com>
2019-12-20 15:30:56 +1100

    Add logging

--- add-comment.sh
+++ add-comment.sh
@@ -20,6 +20,7 @@ AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
 GIF_URL=https://github.com/jessfraz/shaking-finger-action/raw/master/finger.gif
 
 post_gif() {
+	echo "posting gif to PR #${NUMBER}"
 	curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" -d '{"body":"![finger.gif]('${GIF_URL}')"}' -H "Content-Type: application/json" -X POST "${URI}/repos/${GITHUB_REPOSITORY}/issues/${NUMBER}/comments"
 }
 
@@ -27,7 +28,11 @@ main() {
 	# Get the pull request number.
 	NUMBER=$(jq --raw-output .number "$GITHUB_EVENT_PATH")
 
+	echo "running $GITHUB_ACTION for PR #${NUMBER}"
+
 	post_gif;
+
+	echo "execution finished"
 }
 
 main
